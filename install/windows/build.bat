2f9d368beb4f61fee476b05d33d0c1ad0cce92f8
Micah Lee <micah@micahflee.com>
2020-02-24 16:51:59 -0800

    Add code signing to windows release

--- install/windows/build.bat
+++ install/windows/build.bat
@@ -5,7 +5,8 @@ rmdir /s /q build
 REM build the exe with pyinstaller
 pyinstaller install\pyinstaller\pyinstaller.spec
 
-REM TODO: code sign dangerzone.exe
+REM code sign dangerzone.exe
+signtool.exe sign /v /d "Dangerzone" /a /tr http://time.certum.pl/ dist\dangerzone\dangerzone.exe
 
 REM build the msi package
 cd build
@@ -14,8 +15,9 @@ cd wix
 candle.exe ..\..\install\windows\Dangerzone.wxs
 light.exe -ext WixUIExtension Dangerzone.wixobj
 
-REM TODO: code sign dangerzone.msi
+REM code sign dangerzone.msi
+signtool.exe sign /v /d "Dangerzone" /a /tr http://time.certum.pl/ build\wix\Dangerzone.msi
 
-REM moving dangerzone.msi to dist
+REM moving Dangerzone.msi to dist
 cd ..\..
 move build\wix\Dangerzone.msi dist

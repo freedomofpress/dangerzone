c39562906eaeee816fb8c282727c5e383ac1fe41
Micah Lee <micah@micahflee.com>
2021-06-25 12:20:42 -0700

    Extract vmlinuz and initramfs as well

--- rip_docker/vm-image-builder/build.sh
+++ rip_docker/vm-image-builder/build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+
+# Install dependencies
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y qemu-utils wget nbdfuse
+
+# Build the VM image
 cd /build
+mkdir -p /build/out
 ./alpine-make-vm-image \
     --image-format qcow2 \
     --image-size 2G \
     --packages "$(cat packages)" \
     --script-chroot \
-    dangerzone.qcow2 -- ./configure.sh
+    /build/out/dangerzone.qcow2 -- ./configure.sh
+
+# Extract vmlinuz and initramfs
+qemu-nbd -c /dev/nbd0 /build/out/dangerzone.qcow2
+mount /dev/nbd0 /mnt
+cp /mnt/boot/vmlinuz-virt /build/out
+cp /mnt/boot/initramfs-virt /build/out
+umount /mnt

ad5de25e76593bbedbb7253fc5e78a445e3e57c1
Micah Lee <micah@micahflee.com>
2021-06-28 11:18:02 -0700

    Rename out to vm

--- rip_docker/vm-image-builder/build.sh
+++ rip_docker/vm-image-builder/build.sh
@@ -7,17 +7,18 @@ apt-get install -y qemu-utils wget nbdfuse
 
 # Build the VM image
 cd /build
-mkdir -p /build/out
+rm -r /build/vm
+mkdir -p /build/vm
 ./alpine-make-vm-image \
     --image-format qcow2 \
     --image-size 2G \
     --packages "$(cat packages)" \
     --script-chroot \
-    /build/out/dangerzone.qcow2 -- ./configure.sh
+    /build/vm/dangerzone.qcow2 -- ./configure.sh
 
 # Extract vmlinuz and initramfs
-qemu-nbd -c /dev/nbd0 /build/out/dangerzone.qcow2
+qemu-nbd -c /dev/nbd0 /build/vm/dangerzone.qcow2
 mount /dev/nbd0 /mnt
-cp /mnt/boot/vmlinuz-virt /build/out
-cp /mnt/boot/initramfs-virt /build/out
+cp /mnt/boot/vmlinuz-virt /build/vm
+cp /mnt/boot/initramfs-virt /build/vm
 umount /mnt
--- rip_docker/vm-image-builder/run-vm.sh
+++ rip_docker/vm-image-builder/run-vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT=$(pwd)/out
+ROOT=$(pwd)/vm
 
 echo "[] Running vpnkit"
 VPNKIT_SOCK=$ROOT/vpnkit.eth.sock
@@ -9,8 +9,10 @@ vpnkit --ethernet=$VPNKIT_SOCK &
 echo $! > $PIDFILE
 trap 'test -f $PIDFILE && kill `cat $PIDFILE` && rm $PIDFILE' EXIT
 
-echo "[] Making disk image"
-mkfile 1g $ROOT/disk.img
+sleep 1
+
+# echo "[] Making disk image"
+# mkfile 1g $ROOT/disk.img
 
 echo "[] Starting VM"
 hyperkit \
@@ -19,8 +21,9 @@ hyperkit \
     -c 2 \
     -s 0:0,hostbridge -s 31,lpc \
     -l com1,stdio \
+    -s 1:0,ahci-hd,file://$ROOT/dangerzone.qcow2,format=qcow \
     -s 2:0,virtio-vpnkit,path=$VPNKIT_SOCK \
-    -s 3:0,ahci-hd,file://$ROOT/dangerzone.qcow2,format=qcow \
-    -s 4:0,virtio-blk,$ROOT/disk.img \
     -U 9efa82d7-ebd5-4287-b1cc-ac4160a39fa7 \
     -f kexec,$ROOT/vmlinuz-virt,$ROOT/initramfs-virt,"earlyprintk=serial console=ttyS0 modules=loop,squashfs,sd-mod,usb-storage"
+
+    # -s 4:0,virtio-blk,$ROOT/disk.img \

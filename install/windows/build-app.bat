bc8b9d62b4ed1e6e3946bce264bac778c5aedb40
Micah Lee <micah@micahflee.com>
2021-12-13 15:23:45 -0800

    Switch Windows package from pyinstaller to cx_freeze

--- install/windows/build-app.bat
+++ install/windows/build-app.bat
@@ -2,25 +2,26 @@ REM delete old dist and build files
 rmdir /s /q dist
 rmdir /s /q build
 
-REM build the exe with pyinstaller
-pyinstaller install\pyinstaller\pyinstaller.spec
+REM build the exe
+python .\setup-windows.py build
 
 REM code sign dangerzone.exe
-signtool.exe sign /v /d "Dangerzone" /sha1 28a4af3b6ba5ed0ef307e1b96a140e1b42450c3b /tr http://timestamp.digicert.com dist\dangerzone\dangerzone.exe
+signtool.exe sign /v /d "Dangerzone" /sha1 28a4af3b6ba5ed0ef307e1b96a140e1b42450c3b /tr http://timestamp.digicert.com build\exe.win32-3.9\dangerzone.exe
+signtool.exe sign /v /d "Dangerzone" /sha1 28a4af3b6ba5ed0ef307e1b96a140e1b42450c3b /tr http://timestamp.digicert.com build\exe.win32-3.9\dangerzone-cli.exe
 
 REM build the wix file
-python install\windows\build-wxs.py > install\windows\Dangerzone.wxs
+python install\windows\build-wxs.py > build\Dangerzone.wxs
 
 REM build the msi package
 cd build
-mkdir wix
-cd wix
-candle.exe ..\..\install\windows\Dangerzone.wxs
+candle.exe Dangerzone.wxs
 light.exe -ext WixUIExtension Dangerzone.wixobj
 
 REM code sign dangerzone.msi
+insignia.exe -im Dangerzone.msi
 signtool.exe sign /v /d "Dangerzone" /sha1 28a4af3b6ba5ed0ef307e1b96a140e1b42450c3b /tr http://timestamp.digicert.com Dangerzone.msi
 
 REM moving Dangerzone.msi to dist
-cd ..\..
-move build\wix\Dangerzone.msi dist
+cd ..
+mkdir dist
+move build\Dangerzone.msi dist
---
+++ setup-windows.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+from cx_Freeze import setup, Executable
+
+with open("share/version.txt") as f:
+    version = f.read().strip()
+
+
+setup(
+    name="dangerzone",
+    version=version,
+    description="Take potentially dangerous PDFs, office documents, or images and convert them to a safe PDF",
+    options={
+        "build_exe": {
+            "packages": ["dangerzone", "dangerzone.gui"],
+            "excludes": ["test", "tkinter"],
+            "include_files": [("share", "share"), ("LICENSE", "LICENSE")],
+            "include_msvcr": True,
+        }
+    },
+    executables=[
+        Executable("install/windows/dangerzone.py", base="Win32GUI"),
+        Executable("install/windows/dangerzone-cli.py", base=None),
+    ],
+)

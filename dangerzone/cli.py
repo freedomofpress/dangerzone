freedomofpress/dangerzone

commit 95ed34626dd744c946644c6df7fa08a4145ac6a3

fix type hint in checking if output files exist

dangerzone/cli.py
@@ -52,7 +52,7 @@ def cli_main(
             return
 
         try:
-            with open(os.path.abspath(output_filename), "wb") as f:
+            with open(os.path.abspath(output_filename), "rb") as f:
                 pass
         except:
             valid = False
@@ -68,7 +68,7 @@ def cli_main(
             f"{os.path.splitext(common.input_filename)[0]}-safe.pdf"
         )
         try:
-            with open(common.output_filename, "wb") as f:
+            with open(common.output_filename, "rb") as f:
                 pass
         except:
             click.echo(

commit 6b385abfef75d165de92b25be465afb03f126812

fix regression: --output-filename fails
--output-filename failed with the message:

   Safe PDF filename is not writable

Bug introduced in commit 95ed346.

dangerzone/cli.py
@@ -52,7 +52,7 @@ def cli_main(
             return
 
         try:
-            with open(os.path.abspath(output_filename), "rb") as f:
+            with open(os.path.abspath(output_filename), "wb"):
                 pass
         except:
             valid = False
@@ -68,7 +68,7 @@ def cli_main(
             f"{os.path.splitext(common.input_filename)[0]}-safe.pdf"
         )
         try:
-            with open(common.output_filename, "rb") as f:
+            with open(common.output_filename, "wb"):
                 pass
         except:
             click.echo(
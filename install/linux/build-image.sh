4eead90c00128e7a7d13e12530790af951f3464f
Alex Pyrgiotis <alex.p@freedom.press>
2022-11-29 15:12:21 +0200

    install: Fail early when image build fails

--- install/linux/build-image.sh
+++ install/linux/build-image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 echo "Building container image"
 podman build container --platform linux/amd64 --tag dangerzone.rocks/dangerzone
 
--- install/macos/build-image.sh
+++ install/macos/build-image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 echo "Building container image"
 docker build container --platform linux/amd64 --tag dangerzone.rocks/dangerzone
 

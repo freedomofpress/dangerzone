23bee23d814c63ecaebf43f98e4e8e9a6c8a110b
deeplow <deeplower@protonmail.com>
2023-09-22 08:37:16 +0100

    Disable isolation_provider tests on dummy conversion
    Windows and macOS in CI (which don't support nested virtualization)
    and thus Docker aren't really candidates for isolation_provider tests.

--- tests/isolation_provider/base.py
+++ tests/isolation_provider/base.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from colorama import Style
 from pytest_mock import MockerFixture
@@ -5,10 +7,15 @@ from pytest_mock import MockerFixture
 from dangerzone.conversion import errors
 from dangerzone.document import Document
 from dangerzone.isolation_provider import base
+from dangerzone.isolation_provider.qubes import running_on_qubes
 
 from .. import pdf_11k_pages, sanitized_text, uncommon_text
 
 
+@pytest.mark.skipif(
+    os.environ.get("DUMMY_CONVERSION", False), reason="dummy conversions not supported"
+)
+@pytest.mark.skipif(not running_on_qubes(), reason="Not on a Qubes system")
 class IsolationProviderTest:
     def test_print_progress(
         self,

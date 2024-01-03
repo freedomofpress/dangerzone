e2531279c0cbc5677655fedcf20f470539e1eb01
deeplow <deeplower@protonmail.com>
2024-01-03 08:52:42 +0000

    FIXUP Revert "Disable image compression when saving PDF"
    This reverts commit f074db0beaa50389634203657f9b46307164a353.

--- dangerzone/conversion/pixels_to_pdf.py
+++ dangerzone/conversion/pixels_to_pdf.py
@@ -86,7 +86,7 @@ class PixelsToPDF(DangerzoneConverter):
         else:
             safe_pdf_path = f"/safezone/safe-output-compressed.pdf"
 
-        safe_doc.save(safe_pdf_path)
+        safe_doc.save(safe_pdf_path, deflate_images=True)
 
 
 async def main() -> int:

d5ccee30f9623be3c85c18b0b2cdf3b6d6184c8a
Alexis Métaireau <alexis@notmyidea.org>
2024-05-22 15:27:52 +0200

    refactor: use pathlib `/` separator rather than `.joinpath`
    Mainly to help readability

--- dangerzone/util.py
+++ dangerzone/util.py
@@ -27,21 +27,21 @@ def get_resource_path(filename: str) -> str:
     if getattr(sys, "dangerzone_dev", False):
         # Look for resources directory relative to python file
         project_root = pathlib.Path(__file__).parent.parent
-        prefix = project_root.joinpath("share")
+        prefix = project_root / "share"
     else:
         if platform.system() == "Darwin":
             bin_path = pathlib.Path(sys.executable)
             app_path = bin_path.parent.parent
-            prefix = app_path.joinpath("Resources", "share")
+            prefix = app_path / "Resources" / "share"
         elif platform.system() == "Linux":
-            prefix = pathlib.Path(sys.prefix).joinpath("share", "dangerzone")
+            prefix = pathlib.Path(sys.prefix) / "share" / "dangerzone"
         elif platform.system() == "Windows":
             exe_path = pathlib.Path(sys.executable)
             dz_install_path = exe_path.parent
-            prefix = dz_install_path.joinpath("share")
+            prefix = dz_install_path / "share"
         else:
             raise NotImplementedError(f"Unsupported system {platform.system()}")
-    resource_path = prefix.joinpath(filename)
+    resource_path = prefix / filename
     return str(resource_path)
 
 

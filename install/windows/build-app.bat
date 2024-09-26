4abd4720be343b7994745f4ac78067d1217e34ee
JKarasti <juho.karasti@gmail.com>
2024-09-26 16:20:23 +0300

    Change: Verify the signatures of the signed files with `signtool verify`

--- install/windows/build-app.bat
+++ install/windows/build-app.bat
@@ -8,8 +8,14 @@ python .\setup-windows.py build
 REM code sign dangerzone.exe
 signtool.exe sign /v /d "Dangerzone" /a /n "Freedom of the Press Foundation" /fd sha256 /t http://time.certum.pl/ build\exe.win-amd64-3.12\dangerzone.exe
 
+REM verify the signature of dangerzone.exe
+signtool.exe verify /pa build\exe.win-amd64-3.12\dangerzone.exe
+
 REM code sign dangerzone-cli.exe
 signtool.exe sign /v /d "Dangerzone" /a /n "Freedom of the Press Foundation" /fd sha256 /t http://time.certum.pl/ build\exe.win-amd64-3.12\dangerzone-cli.exe
+
+REM verify the signature of dangerzone-cli.exe
+signtool.exe verify /pa build\exe.win-amd64-3.12\dangerzone-cli.exe
 
 REM build the wix file
 python install\windows\build-wxs.py > build\Dangerzone.wxs
@@ -22,6 +28,9 @@ light.exe -ext WixUIExtension Dangerzone.wixobj
 REM code sign Dangerzone.msi
 insignia.exe -im Dangerzone.msi
 signtool.exe sign /v /d "Dangerzone" /a /n "Freedom of the Press Foundation" /fd sha256 /t http://time.certum.pl/ Dangerzone.msi
+
+REM verify the signature of Dangerzone.msi
+signtool.exe verify /pa Dangerzone.msi
 
 REM moving Dangerzone.msi to dist
 cd ..

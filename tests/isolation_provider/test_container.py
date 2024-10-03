130ba92e3470466f4088bc4e6e7a385a7e6decd9
Alex Pyrgiotis <alex.p@freedom.press>
2024-10-03 14:30:13 +0300

    WIP: Remove all provider wait lines

--- tests/isolation_provider/test_container.py
+++ tests/isolation_provider/test_container.py
@@ -21,31 +21,6 @@ def provider() -> Container:
     return Container()
 
 
-class ContainerWait(Container):
-    """Container isolation provider that blocks until the container has started."""
-
-    def exec_container(self, *args, **kwargs):  # type: ignore [no-untyped-def]
-        # Check every 100ms if a container with the expected name has showed up.
-        # Else, closing the file descriptors may not work.
-        name = kwargs["name"]
-        runtime = self.get_runtime()
-        p = super().exec_container(*args, **kwargs)
-        for i in range(50):
-            containers = subprocess.run(
-                [runtime, "ps"], capture_output=True
-            ).stdout.decode()
-            if name in containers:
-                return p
-            time.sleep(0.1)
-
-        raise RuntimeError(f"Container {name} did not start within 5 seconds")
-
-
-@pytest.fixture
-def provider_wait() -> ContainerWait:
-    return ContainerWait()
-
-
 class TestContainer(IsolationProviderTest):
     pass
 

91fbc466c5db2d02c839cd7e89fa5b0b11fb74b3
Alex Pyrgiotis <alex.p@freedom.press>
2024-10-07 14:45:09 +0300

    Add an import preference for vendored packages
    Prefer importing packages from ./dangerzone/vendor, if there is one
    there, instead of using the system ones.

--- dangerzone/__init__.py
+++ dangerzone/__init__.py
@@ -1,5 +1,18 @@
+import logging
 import os
 import sys
+
+logger = logging.getLogger(__name__)
+
+
+try:
+    from . import vendor  # type: ignore [attr-defined]
+
+    vendor_path: str = vendor.__path__[0]
+    logger.debug(f"Using vendored PyMuPDF libraries from '{vendor_path}'")
+    sys.path.insert(0, vendor_path)
+except ImportError:
+    pass
 
 if "DANGERZONE_MODE" in os.environ:
     mode = os.environ["DANGERZONE_MODE"]

25ac980b0b5dd606e5f836b2c7557472f254773c
Alex Pyrgiotis <alex.p@freedom.press>
2024-10-08 18:56:27 +0300

    FIXUP: Fix for vendoring PyMuPDF

--- install/linux/vendor-pymupdf.py
+++ install/linux/vendor-pymupdf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 import subprocess
 import sys
 from pathlib import Path
@@ -21,6 +22,10 @@ def main():
     cmd = ["poetry", "export", "--only", "container"]
     container_requirements_txt = subprocess.check_output(cmd)
 
+    # XXX: Hack for Ubuntu Focal.
+    if sys.version.startswith("3.8"):
+        container_requirements_txt = container_requirements_txt.replace(b"3.9", b"3.8")
+
     print(f">>> Vendoring PyMuPDF under '{args.dest}'", file=sys.stderr)
     # We prefer to call the CLI version of `pip`, instead of importing it directly, as
     # instructed here:
@@ -37,7 +42,11 @@ def main():
         "--requirement",
         "/proc/self/fd/0",  # XXX: pip does not read requirements.txt from stdin
     ]
-    subprocess.check_output(cmd, input=container_requirements_txt)
+    subprocess.run(cmd, check=True, input=container_requirements_txt)
+
+    if not os.listdir(args.dest):
+        print(f">>> Failed to vendor PyMuPDF under '{args.dest}'", file=sys.stderr)
+
 
     print(f">>> Successfully vendored PyMuPDF under '{args.dest}'", file=sys.stderr)
 

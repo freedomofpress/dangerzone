767bfa7e48d42f90b663092b3f20e17f5336bc77
jkarasti <juho.karasti@gmail.com>
2024-11-25 19:11:25 +0200

    Lint: Fix unused-variable (F841)

--- dangerzone/logic.py
+++ dangerzone/logic.py
@@ -71,7 +71,7 @@ class DangerzoneCore(object):
                     ocr_lang,
                     stdout_callback,
                 )
-            except Exception as e:
+            except Exception:
                 log.exception(
                     f"Unexpected error occurred while converting '{document}'"
                 )

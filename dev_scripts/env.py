1298e9c3981d160d63a5992ba97521c644b1cbd5
Alexis Métaireau <alexis@freedom.press>
2025-01-06 14:20:06 +0100

    build: add `build_scripts/env.py` to the hashed files
    It contains information that define the build environments, and as such, modifying it should result in a new release of the dev containers.

--- dev_scripts/env.py
+++ dev_scripts/env.py
@@ -292,6 +292,7 @@ def get_build_dir_sources(distro, version):
     sources = [
         git_root() / "pyproject.toml",
         git_root() / "poetry.lock",
+        git_root() / "dev_scripts" / "env.py",
         git_root() / "dev_scripts" / "storage.conf",
         git_root() / "dev_scripts" / "containers.conf",
     ]

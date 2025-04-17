9d4ffbc7f15f097c81416ba8fdac67e0b746a5c4
Alexis Métaireau <alexis@freedom.press>
2025-04-17 17:19:04 +0200

    dangerzone.updater exposes a few funtions, constants and exceptions
    This is done to avoid looking at the internal logic of
    `dangerzone.updater`. Only the features that actually are part of
    the exposed API are exposed, and do not require deep knowledge of the
    updater's logic to be used.

--- dangerzone/updater/__init__.py
+++ dangerzone/updater/__init__.py
@@ -1,3 +1,12 @@
 import logging
 
 log = logging.getLogger(__name__)
+
+from .errors import SignatureError, UpdaterError
+from .signatures import (
+    DEFAULT_PUBKEY_LOCATION,
+    install_local_container_tar,
+    is_update_available,
+    upgrade_container_image,
+    verify_local_image,
+)

55934619ea1c2b8ae8059963db43f836d6c666b1
Alexis Métaireau <alexis@freedom.press>
2025-06-11 13:42:34 +0200

    build: Vendor cosign and update packaging
    Add cosign binaries and update Debian/Fedora packaging

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ def data_files_list():
             ["install/linux/press.freedom.dangerzone.png"],
         ),
         ("share/dangerzone", file_list("share")),
+        ("share/dangerzone/vendor", file_list("share/vendor")),
     ]
     return data_files
 
@@ -49,6 +50,7 @@ convert the documents within a secure sandbox.
         "dangerzone.conversion",
         "dangerzone.gui",
         "dangerzone.isolation_provider",
+        "dangerzone.updater",
     ],
     data_files=data_files_list(),
     classifiers=[
@@ -60,6 +62,7 @@ convert the documents within a secure sandbox.
         "console_scripts": [
             "dangerzone = dangerzone:main",
             "dangerzone-cli = dangerzone:main",
+            "dangerzone-image = dangerzone.updater.cli:main",
         ]
     },
 )

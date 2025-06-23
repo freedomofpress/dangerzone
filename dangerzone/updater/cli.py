01721a26e9b56044decf73af6550cf5d0a50027b
Alexis Métaireau <alexis@freedom.press>
2025-06-23 12:18:45 +0200

    Use DEFAULT_IMAGE wherever possible

--- dangerzone/updater/cli.py
+++ dangerzone/updater/cli.py
@@ -32,18 +32,20 @@ def main(debug: bool) -> None:
 def upgrade() -> None:
     """Upgrade the image to the latest version (only if it is signed).
 
-    It is not possible to upgrade to a different image than the one specific in share/image-name.txt using this CLI invocation.
-    If you want to do so, please use "prepare-archive" and "load-archive" instead.
+    It is not possible to upgrade to a different image than the one
+    specified in share/image-name.txt using this CLI invocation.
+
+    If you want to do so, please use "prepare-archive" and "load-archive"
+    instead.
     """
-    image = expected_image_name()
-    manifest_digest = registry.get_manifest_digest(image)
+    manifest_digest = registry.get_manifest_digest(DEFAULT_IMAGE_NAME)
 
     try:
         callback = functools.partial(click.echo, nl=False)
-        signatures.upgrade_container_image(manifest_digest, image, callback=callback)
-        click.echo(f"✅ The local image {image} has been upgraded")
+        signatures.upgrade_container_image(manifest_digest, DEFAULT_IMAGE_NAME, callback=callback)
+        click.echo(f"✅ The local image {DEFAULT_IMAGE_NAME} has been upgraded")
         click.echo(f"✅ The image has been signed with {DEFAULT_PUBKEY_LOCATION}")
-        click.echo(f"✅ Signatures has been verified and stored locally")
+        click.echo(f"✅ Signatures have been verified and stored locally")
 
     except errors.ImageAlreadyUpToDate as e:
         click.echo(f"✅ {e}")
@@ -60,20 +62,20 @@ def store_signatures(image: str) -> None:
     sigs = signatures.get_remote_signatures(image, manifest_digest)
     signatures.verify_signatures(sigs, manifest_digest)
     signatures.store_signatures(sigs, manifest_digest, update_logindex=False)
-    click.echo(f"✅ Signatures has been verified and stored locally")
+    click.echo(f"✅ Signatures have been verified and stored locally")
 
 
 @main.command()
-@click.argument("image_filename", type=click.Path(exists=True))
+@click.argument("archive_filename", type=click.Path(exists=True))
 @click.option("--force", is_flag=True)
-def load_archive(image_filename: Path, force: bool) -> None:
+def load_archive(archive_filename: Path, force: bool) -> None:
     """Upgrade the local image to the one in the archive."""
     try:
         loaded_image = signatures.upgrade_container_image_airgapped(
-            image_filename, bypass_logindex=force
+            archive_filename, bypass_logindex=force
         )
         click.echo(
-            f"✅ Installed image {image_filename} on the system as {loaded_image}"
+            f"✅ Installed image {archive_filename} on the system as {loaded_image}"
         )
     except errors.ImageAlreadyUpToDate as e:
         click.echo(f"✅ {e}")
@@ -83,18 +85,18 @@ def load_archive(image_filename: Path, force: bool) -> None:
 
 
 @main.command()
-@click.option("--image", default=expected_image_name())
-@click.option("--output-template", default="dangerzone-{arch}.tar")
+@click.option("--image", default=DEFAULT_IMAGE_NAME)
+@click.option("--output", default="dangerzone-{arch}.tar")
 @click.option(
     "--arch",
     default=get_architecture(),
     help="The architecture you want to prepare the archive for. By default, it uses your platform.",
 )
-def prepare_archive(image: str, output_template: str, arch: str) -> None:
+def prepare_archive(image: str, output: str, arch: str) -> None:
     """Prepare an archive to upgrade the dangerzone image (useful for airgapped environment)"""
-    output = output_template.format(arch=arch)
-    signatures.prepare_airgapped_archive(image, output, arch)
-    click.echo(f"✅ Archive {output} created")
+    archive = output.format(arch=arch)
+    signatures.prepare_airgapped_archive(image, archive, arch)
+    click.echo(f"✅ Archive {archive} created")
 
 
 @main.command()
@@ -116,7 +118,7 @@ def verify_local(image: str, pubkey: Path) -> None:
 
 
 @main.command()
-@click.argument("image")
+@click.argument("image", default=DEFAULT_IMAGE_NAME)
 def list_remote_tags(image: str) -> None:
     """List the tags available for a given image."""
     click.echo(f"Existing tags for {image}")

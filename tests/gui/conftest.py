fdaeab8bf86c6b1c1dd611e0c9e6bf1c79e07212
Alex Pyrgiotis <alex.p@freedom.press>
2025-08-05 15:04:47 +0300

    gui: Run startup tasks in background thread
    Replace the various threads we had for checking for updates and
    installing container images, with a single thread that runs in the
    background and performs some startup tasks.
    
    Also, connect each event from these startup tasks with handler of the
    main window, in order to perform an action for every conceivable task
    transition.
    
    Fixes #118
    Fixes #1045
    Fixes #1145
    Fixes #1171
    Fixes #1184
    Closes #253
    Closes #470
    Closes #1132

--- tests/gui/conftest.py
+++ tests/gui/conftest.py
@@ -4,42 +4,19 @@ from typing import Optional
 import pytest
 from pytest import MonkeyPatch
 from pytest_mock import MockerFixture
+from pytestqt.qtbot import QtBot
 
 from dangerzone import util
 from dangerzone.gui import Application
 from dangerzone.gui.logic import DangerzoneGui
-from dangerzone.gui.updater import UpdaterThread
+from dangerzone.gui.main_window import MainWindow
 from dangerzone.isolation_provider.dummy import Dummy
 
 
-def get_qt_app() -> Application:
-    if Application.instance() is None:  # type: ignore [call-arg]
-        return Application()
-    else:
-        return Application.instance()  # type: ignore [call-arg]
-
-
-def generate_isolated_updater(
-    tmp_path: Path,
-    mocker: MockerFixture,
-    mock_app: bool = False,
-) -> UpdaterThread:
-    """Generate an Updater class with its own settings."""
-    app = mocker.MagicMock() if mock_app else get_qt_app()
-
-    dummy = Dummy()
-    mocker.patch("dangerzone.settings.get_config_dir", return_value=tmp_path)
-
-    dangerzone = DangerzoneGui(app, isolation_provider=dummy)
-    updater = UpdaterThread(dangerzone)
-    return updater
-
-
 @pytest.fixture
-def updater(tmp_path: Path, mocker: MockerFixture) -> UpdaterThread:
-    return generate_isolated_updater(tmp_path, mocker, mock_app=True)
-
-
-@pytest.fixture
-def qt_updater(tmp_path: Path, mocker: MockerFixture) -> UpdaterThread:
-    return generate_isolated_updater(tmp_path, mocker, mock_app=False)
+def dangerzone_gui(
+    qtbot: QtBot, mocker: MockerFixture, tmp_path: Path
+) -> DangerzoneGui:
+    mock_app = mocker.MagicMock()
+    dummy = mocker.MagicMock(spec=Dummy)
+    return DangerzoneGui(mock_app, dummy)

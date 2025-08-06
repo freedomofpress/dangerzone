bb41fce0073000677fbac35a53a3cd25e16cb9be
Alex Pyrgiotis <alex.p@freedom.press>
2025-08-06 17:42:33 +0300

    podman: Add note about replacing this module

--- dangerzone/podman/command/__init__.py
+++ dangerzone/podman/command/__init__.py
@@ -1,3 +1,8 @@
+# TODO: Consider replacing this module with an official package, once this code gets
+# merged to `containers/podman-py`.
+#
+# See https://github.com/freedomofpress/dangerzone/issues/1227
+
 from .cli_runner import GlobalOptions
 from .command import PodmanCommand
 

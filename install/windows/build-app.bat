5b845d104c3ccdf9e34e4412882e7fa1174174c9
Alex Pyrgiotis <alex.p@freedom.press>
2025-11-05 22:49:42 +0200

    WIP: Comment rm dist/

--- install/windows/build-app.bat
+++ install/windows/build-app.bat
@@ -1,23 +1,23 @@
-REM delete old dist and build files
-rmdir /s /q dist
-rmdir /s /q build
+REM REM delete old dist and build files
+REM rmdir /s /q dist
+REM rmdir /s /q build
 
-REM build the gui and cli exe
-python .\setup-windows.py build
+REM REM build the gui and cli exe
+REM python .\setup-windows.py build
 
-REM code sign executables
-signtool.exe sign /v /d "Dangerzone" /a /n "Freedom of the Press Foundation" /fd sha256 /t http://time.certum.pl/ ^
- build\exe.win-amd64-3.13\dangerzone.exe ^
- build\exe.win-amd64-3.13\dangerzone-cli.exe ^
- build\exe.win-amd64-3.13\dangerzone-image.exe ^
- build\exe.win-amd64-3.13\dangerzone-machine.exe
+REM REM code sign executables
+REM signtool.exe sign /v /d "Dangerzone" /a /n "Freedom of the Press Foundation" /fd sha256 /t http://time.certum.pl/ ^
+REM  build\exe.win-amd64-3.13\dangerzone.exe ^
+REM  build\exe.win-amd64-3.13\dangerzone-cli.exe ^
+REM  build\exe.win-amd64-3.13\dangerzone-image.exe ^
+REM  build\exe.win-amd64-3.13\dangerzone-machine.exe
 
-REM verify the signatures of the executables
-signtool.exe verify /pa ^
- build\exe.win-amd64-3.13\dangerzone.exe ^
- build\exe.win-amd64-3.13\dangerzone-cli.exe ^
- build\exe.win-amd64-3.13\dangerzone-image.exe ^
- build\exe.win-amd64-3.13\dangerzone-machine.exe
+REM REM verify the signatures of the executables
+REM signtool.exe verify /pa ^
+REM  build\exe.win-amd64-3.13\dangerzone.exe ^
+REM  build\exe.win-amd64-3.13\dangerzone-cli.exe ^
+REM  build\exe.win-amd64-3.13\dangerzone-image.exe ^
+REM  build\exe.win-amd64-3.13\dangerzone-machine.exe
 
 REM build the wxs file
 python install\windows\build-wxs.py

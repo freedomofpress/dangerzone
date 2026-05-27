48d03a6be90a9690ac49b1728ebee64f17365ebe
Alexis M <alexis@notmyidea.org>
2026-05-27 18:52:03 +0200

    tests: Load and tag an image before running tests
    This wasn't failing previously because of the order in which the tests
    were running. This makes sure that an image is present and with the
    proper tags before running the tests.

--- tests/isolation_provider/test_container.py
+++ tests/isolation_provider/test_container.py
@@ -1,11 +1,12 @@
 import os
 import platform
+from typing import Generator
 
 import pytest
 from pytest_mock import MockerFixture
 from pytest_subprocess import FakeProcess
 
-from dangerzone import errors
+from dangerzone import container_utils, errors
 from dangerzone.container_utils import expected_image_name, init_podman_command
 from dangerzone.isolation_provider.container import Container
 from dangerzone.isolation_provider.qubes import is_qubes_native_conversion
@@ -24,6 +25,28 @@ elif os.environ.get("DUMMY_CONVERSION", False):
     pytest.skip("Dummy conversion is enabled", allow_module_level=True)
 
 
+@pytest.fixture(scope="session", autouse=True)
+def ensure_container_image() -> Generator[None, None, None]:
+    """Ensure an image is loaded and tagged propertly before running tests"""
+    on_podman_machine = platform.system() != "Linux"
+    if on_podman_machine:
+        m = machine.PodmanMachineManager()
+        m.init()
+        m.start()
+
+    image_name = expected_image_name()
+    try:
+        container_utils.get_local_image_digest()
+    except errors.ImageNotPresentException:
+        image_id = container_utils.load_image_tarball()
+        init_podman_command().run(["tag", image_id, image_name])
+
+    yield
+
+    if on_podman_machine:
+        machine.PodmanMachineManager().stop()
+
+
 @pytest.fixture
 def provider(skip_image_verification: None) -> Container:
     return Container()
@@ -35,30 +58,8 @@ def runtime_path() -> str:
 
 
 class TestContainer(IsolationProviderTest):
-    @classmethod
-    def setup_class(cls) -> None:
-        if platform.system() != "Linux":
-            m = machine.PodmanMachineManager()
-            m.init()
-            m.start()
-
-    @classmethod
-    def teardownn_class(cls) -> None:
-        if platform.system() != "Linux":
-            m = machine.PodmanMachineManager()
-            m.stop()
+    pass
 
 
 class TestContainerTermination(IsolationProviderTermination):
-    @classmethod
-    def setup_class(cls) -> None:
-        if platform.system() != "Linux":
-            m = machine.PodmanMachineManager()
-            m.init()
-            m.start()
-
-    @classmethod
-    def teardownn_class(cls) -> None:
-        if platform.system() != "Linux":
-            m = machine.PodmanMachineManager()
-            m.stop()
+    pass
